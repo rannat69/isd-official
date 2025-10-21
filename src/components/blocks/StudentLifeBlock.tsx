90c09af4effac5f21c33a72893540c274a01979b
Candbot Chan <candbot@outlook.com>
2025-10-21 23:05:42 +0800

    fix: sidebar

--- src/components/blocks/StudentLifeBlock.tsx
+++ src/components/blocks/StudentLifeBlock.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useRouter, useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useSearchParams, useRouter } from 'next/navigation';
+import { JSX, useEffect, useState } from 'react';
 import StudentCompetitionsBlock from './student-life/StudentCompetitionsBlock';
 import ExtraActivitivesBlock from './student-life/ExtraActivitiesBlock';
 import InternshipBlock from './student-life/InternshipBlock';
@@ -9,63 +9,76 @@ import ExchangeBlock from './student-life/ExchangeBlock';
 import AlumniSharingBlock from './student-life/AlumniSharingBlock';
 import UsefulLinksBlock from './student-life/UsefulLinksBlock';
 
-export default function StudentLifeBlock() {
-    const searchParams = useSearchParams();
+type StudentLifePageKey =
+    | 'stud-comp'
+    | 'extra-act'
+    | 'intern'
+    | 'exchange'
+    | 'alumni'
+    | 'links';
 
+export default function StudentLifeBlock() {
+    const pages: {
+        key: StudentLifePageKey;
+        name: string;
+        component: JSX.Element;
+    }[] = [
+        {
+            key: 'stud-comp',
+            name: 'Student Competitions',
+            component: <StudentCompetitionsBlock />,
+        },
+        {
+            key: 'extra-act',
+            name: 'Extracurricular Activities',
+            component: <ExtraActivitivesBlock />,
+        },
+        { key: 'intern', name: 'Internships', component: <InternshipBlock /> },
+        { key: 'exchange', name: 'Exchange', component: <ExchangeBlock /> },
+        {
+            key: 'alumni',
+            name: 'Alumni Sharing',
+            component: <AlumniSharingBlock />,
+        },
+        { key: 'links', name: 'Useful Links', component: <UsefulLinksBlock /> },
+    ];
+
+    const searchParams = useSearchParams();
     const router = useRouter();
-    const [page, setPage] = useState<string>('stud-comp');
+
+    const [page, setPage] = useState<StudentLifePageKey>('stud-comp');
+
     useEffect(() => {
         const initialPage = searchParams.get('page') ?? 'stud-comp';
-        setPage(initialPage);
+        setPage(initialPage as StudentLifePageKey);
     }, [searchParams]);
+
+    const handlePageChange = (newPage: StudentLifePageKey) => {
+        setPage(newPage);
+        const params = new URLSearchParams(searchParams.toString());
+        params.set('page', newPage);
+        router.push(`?${params.toString()}`, { scroll: false });
+    };
 
     return (
         <div className="container py-section-gap flex gap-component-gap">
-            <div className="flex flex-col text-lg cursor-pointer">
-                <div
-                    className={` h-[54px] whitespace-nowrap px-element-gap py-[10px] ${page === 'stud-comp' ? 'font-bold text-isd-primary bg-isd-primary-2 border-l-3 border-isd-primary' : ''}`}
-                    onClick={() => setPage('stud-comp')}
-                >
-                    Student Competitions
-                </div>
-                <div
-                    className={`h-[54px] whitespace-nowrap px-element-gap py-[10px] ${page === 'extra-act' ? 'font-bold text-isd-primary  bg-isd-primary-2 border-l-3 border-isd-primary' : ''}`}
-                    onClick={() => setPage('extra-act')}
-                >
-                    Extracurricular activities
-                </div>
-                <div
-                    className={`h-[54px] whitespace-nowrap px-element-gap py-[10px] ${page === 'intern' ? 'font-bold  text-isd-primary bg-isd-primary-2 border-l-3 border-isd-primary' : ''}`}
-                    onClick={() => setPage('intern')}
-                >
-                    Internships
-                </div>
-                <div
-                    className={`h-[54px] whitespace-nowrap px-element-gap py-[10px] ${page === 'exchange' ? 'font-bold text-isd-primary  bg-isd-primary-2 border-l-3 border-isd-primary' : ''}`}
-                    onClick={() => setPage('exchange')}
-                >
-                    Exchange
-                </div>
-                <div
-                    className={`h-[54px] whitespace-nowrap px-element-gap py-[10px] ${page === 'alumni' ? 'font-bold  text-isd-primary bg-isd-primary-2 border-l-3 border-isd-primary' : ''}`}
-                    onClick={() => setPage('alumni')}
-                >
-                    Alumni Sharing
-                </div>
-                <div
-                    className={`h-[54px] whitespace-nowrap px-element-gap py-[10px] ${page === 'links' ? 'font-bold  text-isd-primary bg-isd-primary-2 border-l-3 border-isd-primary' : ''}`}
-                    onClick={() => setPage('links')}
-                >
-                    Useful Links
-                </div>
+            <div className="flex flex-col text-lg min-w-[324px]">
+                {pages.map((p) => (
+                    <div
+                        key={p.key}
+                        className={
+                            'flex items-center cursor-pointer w-full h-[54px] whitespace-nowrap px-element-gap py-[10px] ' +
+                            (p.key === page && // SCREW CSS BORDER SIZING!
+                                'font-bold text-isd-primary bg-isd-primary-2 border-l-3 pl-[calc(var(--element-gap)-3px)] border-isd-primary')
+                        }
+                        onClick={() => handlePageChange(p.key)}
+                    >
+                        {p.name}
+                    </div>
+                ))}
             </div>
-            <div className=" overflow-y-clip flex flex-col  gap-component-gap-sm text-md leading-[28px] text-isd-font-1">
-                {page === 'stud-comp' && <StudentCompetitionsBlock />}
-                {page === 'extra-act' && <ExtraActivitivesBlock />}
-                {page === 'intern' && <InternshipBlock />}
-                {page === 'exchange' && <ExchangeBlock />}
-                {page === 'alumni' && <AlumniSharingBlock />}
-                {page === 'links' && <UsefulLinksBlock />}
+            <div className="flex flex-col gap-component-gap-sm text-md leading-[28px] text-isd-font-1">
+                {pages.find((p) => p.key === page)?.component}
             </div>
         </div>
     );
