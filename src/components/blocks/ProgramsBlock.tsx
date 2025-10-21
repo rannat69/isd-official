a661e0ac93e6dab1dd0bfde3a836a99b40ae5cf7
Zhang Yunxin <zhang.yunxin@live.com>
2025-10-21 22:30:44 +0800

    fix(people): add back mid page dot pattern
    - not sure why i keep getting the old wrong commit and keep deleting them

--- src/components/blocks/ProgramsBlock.tsx
+++ src/components/blocks/ProgramsBlock.tsx
@@ -34,7 +34,6 @@ export default function ProgramBlock() {
     ];
 
     return (
-        // <div>
         <div className="dot-pattern before:top-[-95px] before:right-[10px] [--dot-color:var(--isd-secondary-1)]">
             <div className="container relative overflow-clip flex flex-col py-section-gap gap-section-title-gap">
                 <h1 className="text-h1 offset-text-background uppercase">
@@ -51,6 +50,10 @@ export default function ProgramBlock() {
                     ))}
                 </div>
             </div>
+            <div
+                className="dot-pattern before:bottom-[25px] before:left-[-90px] [--dot-color:var(--isd-secondary-1)] "
+                aria-hidden
+            />
         </div>
     );
 }
