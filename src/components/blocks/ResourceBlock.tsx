64a8dfbb886571680d3dcbb3cbf992736efa294a
Remi ANNAT <remi_annat@hotmail.com>
2025-10-22 10:27:31 +0800

    Link on whole imagecard

--- src/components/blocks/ResourceBlock.tsx
+++ src/components/blocks/ResourceBlock.tsx
@@ -3,6 +3,7 @@ import ImageCard from '@/components/ImageCard';
 import ScholarshipsImg from '@/assets/scholarships-img.png';
 import ExcangeImg from '@/assets/exchange-img.jpg';
 import FacilitiesImg from '@/assets/facilities-img.png';
+import Link from 'next/link';
 
 export default function ResourceBlock() {
     return (
@@ -11,24 +12,32 @@ export default function ResourceBlock() {
                 Resource
             </h1>
             <div className="grid md:grid-cols-3 grid-cols-1 gap-component-gap-sm">
-                <ImageCard
-                    imageSrc={ScholarshipsImg}
-                    title="Scholarships"
-                    link="/academics/scholarships"
-                    description="Several dedicated partners share our vision of creating new modes of engineering education. As part of our partnership program, they provide valuable resources in forms of financial and in-kind support to the Division."
-                />
-                <ImageCard
-                    imageSrc={ExcangeImg}
-                    title="Internship & Exchange"
-                    link="/academics/internship"
-                    description="ISD students can enjoy a range of internship opportunities offered by our industrial partners, and participate in exchange program coordinated by Academy of Interdisciplinary Studies."
-                />
-                <ImageCard
-                    imageSrc={FacilitiesImg}
-                    title="Facilities"
-                    link="/academics/facilities"
-                    description="The division has two main design and prototyping facilities. The Design Studio is a teaching unit as well as a small 3D printing unit with small hand tools. Most of the first year design classes are held in the Design Studio."
-                />
+                <Link href="/academics/scholarships">
+                    <ImageCard
+                        imageSrc={ScholarshipsImg}
+                        title="Scholarships"
+                        link="/academics/scholarships"
+                        description="Several dedicated partners share our vision of creating new modes of engineering education. As part of our partnership program, they provide valuable resources in forms of financial and in-kind support to the Division."
+                    />
+                </Link>
+
+                <Link href="/academics/internship">
+                    <ImageCard
+                        imageSrc={ExcangeImg}
+                        title="Internship & Exchange"
+                        link="/academics/internship"
+                        description="ISD students can enjoy a range of internship opportunities offered by our industrial partners, and participate in exchange program coordinated by Academy of Interdisciplinary Studies."
+                    />
+                </Link>
+
+                <Link href="/academics/facilities">
+                    <ImageCard
+                        imageSrc={FacilitiesImg}
+                        title="Facilities"
+                        link="/academics/facilities"
+                        description="The division has two main design and prototyping facilities. The Design Studio is a teaching unit as well as a small 3D printing unit with small hand tools. Most of the first year design classes are held in the Design Studio."
+                    />
+                </Link>
             </div>
         </div>
     );
