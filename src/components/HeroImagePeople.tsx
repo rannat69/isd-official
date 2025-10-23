c90ba63bac334949b86f8e707e0677b730e56e20
Candbot Chan <candbot@outlook.com>
2025-10-23 14:26:45 +0800

    fix: don't clip people

--- src/components/HeroImagePeople.tsx
+++ src/components/HeroImagePeople.tsx
@@ -1,12 +1,20 @@
 import Image, { StaticImageData } from 'next/image';
 
-export default function HeroImage({ image }: { image: StaticImageData }) {
+export default function HeroImage({
+    image,
+    people = false,
+}: {
+    image: StaticImageData;
+    people?: boolean;
+}) {
     return (
-        <div className="w-full relative overflow-hidden h-[300px] md:h-[400px] lg:h-[612px]">
+        <div
+            className={`w-full relative overflow-hidden ${people ? 'h-[684px]' : 'h-[612px]'}`}
+        >
             <Image
                 src={image}
                 alt={`Hero Banner Image`}
-                className={`object-cover w-full h-full object-[center_40%]`}
+                className={`object-cover w-full h-full ${people ? 'object-[center]' : 'object-[center]'}`}
             />
         </div>
     );
