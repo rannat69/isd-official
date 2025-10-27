f3401bc68a20380a4990e1c312e8f701274bd96a
Candbot Chan <candbot@outlook.com>
2025-10-27 11:30:16 +0800

    fix: header same breakpoint

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,12 +3,12 @@ import { ChevronUp } from 'lucide-react';
 export default function Header() {
     return (
         <header className="bg-isd-primary text-white py-3 sticky top-0 z-50 w-full">
-            <div className="px-[18px] md:px-section-gap">
+            <div className="px-[18px] xl:px-section-gap">
                 <div className="flex justify-end gap-4 divide-x-1 divide-white items-center ">
-                    <h1 className="text-white text-xs pr-4 hidden md:block">
+                    <h1 className="text-white text-xs pr-4 hidden xl:block">
                         Academy of Interdisciplinary Studies
                     </h1>
-                    <ChevronUp className="lg:stroke-2.5 stroke-2" />
+                    <ChevronUp className="xl:stroke-2.5 stroke-2" />
                 </div>
             </div>
         </header>
