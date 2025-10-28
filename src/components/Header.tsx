3395347136ca8e0859391c68c3fff029ec79932c
Candbot Chan <candbot@outlook.com>
2025-10-29 00:10:29 +0800

    feat: hover underline for AIS

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,10 @@ export default function Header() {
         <header className="bg-isd-primary text-white py-3 sticky top-0 z-50 w-full">
             <div className="px-[18px] xl:px-section-gap">
                 <div className="flex justify-end gap-4 divide-x-1 divide-white items-center ">
-                    <h1 className="text-white text-xs pr-4 hidden xl:block">
+                    <a
+                        href="https://ais.hkust.edu.hk/"
+                        className="text-white text-xs pr-4 hover:underline hidden xl:block"
+                    >
                         Academy of Interdisciplinary Studies
                     </h1>
                     <ChevronUp className="xl:stroke-2.5 stroke-2" />
