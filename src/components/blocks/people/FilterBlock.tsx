4ddbf1c83c38afc23c698a24cd4ace697c65777c
Candbot Chan <candbot@outlook.com>
2025-10-30 12:10:56 +0800

    fix: avoid scroll position changes when selecting a new filter

--- src/components/blocks/people/FilterBlock.tsx
+++ src/components/blocks/people/FilterBlock.tsx
@@ -88,7 +88,7 @@ export default function FilterBlock() {
 
         // if  tag, add #faculty at the end
 
-        router.push(`/people${qs ? `?${qs}` : ''}#select`);
+        router.push(`/people${qs ? `?${qs}` : ''}`, { scroll: false });
     }
 
     function handleSearch() {
