41d066a03bf1b6f8586877f6c9f8155a8f84b1ec
Candbot Chan <candbot@outlook.com>
2025-10-30 14:22:15 +0800

    fix: change cursor to pointer when hovering over position tags

--- src/components/blocks/people/FilterBlock.tsx
+++ src/components/blocks/people/FilterBlock.tsx
@@ -220,7 +220,7 @@ export default function FilterBlock() {
                                     });
                                 }
                             }}
-                            className={`text-sm lg:py-[10px] py-[6px] border rounded-full px-element-gap ${
+                            className={`text-sm cursor-pointer lg:py-[10px] py-[6px] border rounded-full px-element-gap ${
                                 tag === leTag.value
                                     ? 'text-isd-primary font-bold border-2'
                                     : 'text-isd-font-3'
