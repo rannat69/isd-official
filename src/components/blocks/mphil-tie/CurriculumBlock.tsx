9b08483aef89b1d20cb1af74d6cb585091208478
Remi ANNAT <remi_annat@hotmail.com>
2025-11-04 16:37:24 +0800

    Changes MPHIL TIE

--- src/components/blocks/mphil-tie/CurriculumBlock.tsx
+++ src/components/blocks/mphil-tie/CurriculumBlock.tsx
@@ -1,4 +1,5 @@
-import Curriculum from '@/assets/mphil-curriculum.png';
+import Curriculum from '@/assets/academics/mphil-in-tie/TIE.website.png';
+import CurriculumRoadmap from '@/assets/academics/mphil-in-tie/TIE.website.roadmap.png';
 import Image from 'next/image';
 
 export default function CurriculumBlock() {
@@ -10,47 +11,97 @@ export default function CurriculumBlock() {
 
             <div className="flex flex-col gap-component-gap-sm">
                 <p>
-                    Students are required to take a total of 12 credits of
-                    coursework (including 6 credits of core courses, i.e. Design
-                    Thinking, Entrepreneurial Literacy, and 6 credits of
-                    elective courses), to fulfill HKUST graduation requirements,
-                    and complete individual thesis research. Besides the
-                    required courses, students will be equipped with Graduate
-                    Teaching Assistant Training, Professional Development
-                    Courses, and Language courses for full-person development.
-                    The detailed curriculum can be found in the{' '}
+                    <h3 className="text-isd-secondary font-bold">
+                        Program Structure
+                    </h3>
+                    The TIE program is a full-time, 2-year journey
+                    emphasizing&nbsp;
+                    <b>
+                        team-based product/service prototyping, industry
+                        engagement, and individual thesis research.
+                    </b>
+                </p>
+
+                <Image src={Curriculum} alt="Curriculum Timeline" />
+                <Image src={CurriculumRoadmap} alt="Curriculum Roadmap" />
+                <ul>
+                    <li className="flex before:text-center before:content-['•'] leading-[26px] before:min-w-component-gap-sm">
+                        Students form diverse teams and iterate their prototypes
+                        with guidance from prime and supporting supervisors
+                        drawn from faculty and industry They will receive
+                        personalized feedback and resources to enhance their
+                        projects at every stage. Each team demonstrates its
+                        prototype or service and receives opportunities for
+                        commercialization and networking with industrial
+                        partners, ensuring they have the support needed to bring
+                        their innovative ideas to life.
+                    </li>
+
+                    <li className=" flex before:text-center before:content-['•'] leading-[26px] before:min-w-component-gap-sm">
+                        <div>
+                            The first year is funded by a{' '}
+                            <b>guaranteed HKUST postgraduate studentship;</b>{' '}
+                            after the first year, students are expected to seek
+                            external investment through organized pitch days and
+                            investor engagement sessions. Funding for the second
+                            year will be based on securing external support;
+                            students who are unable to obtain this may continue
+                            their studies as part of a standard research MPhil
+                            program.
+                        </div>
+                    </li>
+
+                    <li className=" flex before:text-center before:content-['•'] leading-[26px] before:min-w-component-gap-sm">
+                        <div>
+                            {' '}
+                            The curriculum integrates core modules on Design
+                            Thinking and Entrepreneurial Literacy, broad
+                            elective options (including marine tech, AIoT for
+                            healthcare, and systems design), professional
+                            development, and language courses for holistic
+                            growth. Significant emphasis is placed on industry
+                            workshops, field trips, and seminars for exposure
+                            and skill-building.
+                        </div>
+                    </li>
+                </ul>
+
+                <p>
+                    Students are equipped for real-world challenges: from market
+                    discovery, team formation, MVP development, testing,
+                    pitching, regulatory navigation, fundraising, scaling, and
+                    (ideally) launch—culminating in both team demonstration and
+                    an individual thesis tied to their group’s innovation.
+                </p>
+
+                <p>
+                    To graduate from the TIE programme at HKUST, students must
+                    complete:
+                </p>
+
+                <ul>
+                    <li className="flex before:text-center before:content-['•'] leading-[26px] before:min-w-component-gap-sm">
+                        <b>12 credits of specialized coursework</b> designed to
+                        build core competencies in technology innovation and
+                        entrepreneurship.
+                    </li>
+
+                    <li className="flex before:text-center before:content-['•'] leading-[26px] before:min-w-component-gap-sm">
+                        <b>An independent thesis research project</b>
+                         demonstrating deep technical and entrepreneurial
+                        expertise and original problem-solving.
+                    </li>
+                </ul>
+
+                <p>
+                    For details about the curriculum and specific course
+                    offerings, please visit the&nbsp;
                     <a
                         href="https://prog-crs.hkust.edu.hk/pgprog/2025-26/mphil-tie"
                         className="font-bold text-isd-secondary underline"
                     >
-                        Program Catalog
+                        Program Catalog.
                     </a>
-                    .
-                    <br />
-                    <br />
-                    Besides the required courses, Industrial Engagement
-                    Workshops, Field Trips & Exhibitions, as well as Technology
-                    Innovation and Entrepreneurship Seminar will be provided. 
-                    <br />
-                    <br />
-                    Students in teams of 4 to 5 will collaboratively develop a
-                    product prototype or service throughout the program, and
-                    conduct a product demonstration. Each team will be assigned
-                    a prime supervisor and include additional supervisors based
-                    on the specialized expertise or guidance necessary from
-                    faculty members.
-                    <br />
-                    <br />
-                    Students are required to complete an individual thesis
-                    related to the group-based product prototype or service they
-                    worked on.
-                </p>
-
-                <Image src={Curriculum} alt="Curriculum Timeline" />
-
-                <p>
-                    The 2-year program journey and milestones are illustrated as
-                    above. 
                 </p>
             </div>
         </div>
