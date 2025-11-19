9db0214ad3db85e793ca5c582fa1fbeccabd3bc0
Remi Annat <rannat@free.fr>
2025-11-19 09:59:11 +0800

    Test replace Image with img

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-   // output: 'export', // Enables static export
+    // output: 'export', // Enables static export
     basePath: process.env.CI === 'true' ? '/isd-official' : '',
     assetPrefix: process.env.CI === 'true' ? '/isd-official/' : '',
     trailingSlash: true,
-   /* images: {
+    /* images: {
         unoptimized: true, // Disables image optimization for static export
     },*/
     webpack(config) {
--- src/components/HeroImagePeople.tsx
+++ src/components/HeroImagePeople.tsx
@@ -1,9 +1,9 @@
 import Image, { StaticImageData } from 'next/image';
 
-export default function HeroImage({ image }: { image: StaticImageData }) {
+export default function HeroImage({ image }: { image: string }) {
     return (
         <div className="w-full relative overflow-hidden aspect-[16/9] lg:aspect-[16/7]">
-            <Image
+            <img
                 src={image}
                 alt={`Hero Banner Image`}
                 className="object-cover w-full h-full object-[25%_80%]"
