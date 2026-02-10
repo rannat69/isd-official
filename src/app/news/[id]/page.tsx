6780a0c05ab73edf478640258a486b4da3a244b3
Remi ANNAT <remi_annat@hotmail.com>
2026-02-10 09:26:54 +0800

    Evt 2026.03.26

--- src/app/news/[id]/page.tsx
+++ src/app/news/[id]/page.tsx
@@ -23,8 +23,6 @@ export default async function NewsDetailPage({
     if (!item) return null;
     const images = resolveImages(item.pictures);
 
-  
-
     return (
         <div className="container pt-component-gap-sm pb-section-gap min-h-screen flex flex-col items-stretch">
             <div className="flex flex-col gap-component-gap-sm">
@@ -41,10 +39,6 @@ export default async function NewsDetailPage({
                 >
                     <h1 className="text-h1">{item.title}</h1>
                     <div className="w-full relative">
-
-                            {JSON.stringify(images[0])}
-
-
                         {images.length > 1 ? (
                             <div className="lg:h-[480px] h-[260px]">
                                 <Carousel images={images} />
