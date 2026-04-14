98d284fba2038ab2072e45a3bd94842558653831
Remi ANNAT <remi_annat@hotmail.com>
2026-04-14 11:43:13 +0800

    Redrection admissions

---
+++ src/app/admissions/page.tsx
@@ -0,0 +1,6 @@
+import { redirect } from 'next/navigation';
+
+export default function AdmPage() {
+    redirect('/academics/bsc-in-innovation-design-and-technology-idt');
+    return <p>Redirecting...</p>;
+}
