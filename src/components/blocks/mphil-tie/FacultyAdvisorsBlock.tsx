55accd07691adcfc39d1c684165ec3d36924ea4a
Remi ANNAT <remi_annat@hotmail.com>
2026-04-20 15:58:24 +0800

    Change to svg mphil tie

--- src/components/blocks/mphil-tie/FacultyAdvisorsBlock.tsx
+++ src/components/blocks/mphil-tie/FacultyAdvisorsBlock.tsx
@@ -1,6 +1,3 @@
-import FacAdv1 from '@/assets/academics/mphil-in-tie/fac-adv.1.png';
-import FacAdv1CnSimp from '@/assets/academics/mphil-in-tie/fac-adv.1.cn.simp.png';
-
 import Changyingxiang from '@/assets/academics/mphil-in-tie/fac-adv/changyingxiang.svg';
 import Jacleung from '@/assets/academics/mphil-in-tie/fac-adv/jacleung.svg';
 import Qianzhang from '@/assets/academics/mphil-in-tie/fac-adv/qianzhang.svg';
@@ -32,38 +29,281 @@ export default function FacultyAdvisorsBlock() {
 
             <div className="flex flex-col gap-component-gap-sm">
                 {(language === LANGUAGE_ENGLISH || !language) && (
-                    <>
-                        <Image src={FacAdv1} alt="Faculty and advisors" />
-                        {/*     <div className="flex">
-                            <Image src={Qianzhang} alt="Faculty and advisors" />
-                            <Image src={Zexiangli} alt="Faculty and advisors" />
+                    <div className="flex flex-col  gap-4">
+                        {/* <Image src={FacAdv1} alt="Faculty and advisors" />*/}
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-start max-w-4xl mx-auto px-4">
+                            <div className="flex flex-col items-center text-center">
+                                <Image
+                                    src={Qianzhang}
+                                    alt="Faculty and advisors"
+                                    className=" object-cover h-[250px]"
+                                />
+                                <p className="text-isd-font-3 font-bold">
+                                    Qian ZHANG
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    Head of ISD
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    Tencent Professor of Engineering and Chair
+                                    Professor of CSE
+                                </p>
+                            </div>
+                            <div className="flex flex-col items-center text-center">
+                                <Image
+                                    src={Zexiangli}
+                                    alt="Faculty and advisors"
+                                    className=" object-cover h-[250px]"
+                                />
+                                <p className="text-isd-font-3 font-bold">
+                                    Zexiang LI{' '}
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    &nbsp;
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    Professor, Division of Integrative Systems
+                                    and Design{' '}
+                                </p>
+                            </div>
                         </div>
-                        <div className="flex">
-                            <Image
-                                src={Saikityeung}
-                                alt="Faculty and advisors"
-                            />
-                            <Image
-                                src={Winnieleung}
-                                alt="Faculty and advisors"
-                            />
-                            <Image
-                                src={Changyingxiang}
-                                alt="Faculty and advisors"
-                            />
-                            <Image src={Qijiashao} alt="Faculty and advisors" />
-                            <Image
-                                src={Robscharff}
-                                alt="Faculty and advisors"
-                            />
-
-                            <Image src={Jacleung} alt="Faculty and advisors" />
-                        </div>*/}
-                    </>
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-4 items-start">
+                            <div className="flex flex-col items-center text-center">
+                                <Image
+                                    src={Saikityeung}
+                                    alt="Faculty and advisors"
+                                    className="md:w-full md:h-[220px] h-[200px] object-cover"
+                                />
+                                <p className="text-isd-font-3 font-bold">
+                                    Sai Kit YEUNG
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    &nbsp;
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    Professor
+                                </p>
+                            </div>
+                            <div className="flex flex-col items-center text-center">
+                                <Image
+                                    src={Winnieleung}
+                                    alt="Faculty and advisors"
+                                    className="md:w-full h-[220px] object-cover"
+                                />
+                                <p className="text-isd-font-3 font-bold">
+                                    Winnie LEUNG
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    &nbsp;
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    Associate Professor in Engineering Education
+                                </p>
+                            </div>
+                            <div className="flex flex-col items-center text-center">
+                                <Image
+                                    src={Changyingxiang}
+                                    alt="Faculty and advisors"
+                                    className="md:w-full h-[220px] object-cover"
+                                />
+                                <p className="text-isd-font-3 font-bold">
+                                    Changying XIANG
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    &nbsp;
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    Assistant Professor
+                                </p>
+                            </div>
+                            <div className="flex flex-col items-center text-center">
+                                <Image
+                                    src={Qijiashao}
+                                    alt="Faculty and advisors"
+                                    className="md:w-full h-[220px] object-cover"
+                                />
+                                <p className="text-isd-font-3 font-bold">
+                                    Qijia SHAO
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    &nbsp;
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    Assistant Professor
+                                </p>
+                            </div>
+                            <div className="flex flex-col items-center text-center">
+                                <Image
+                                    src={Robscharff}
+                                    alt="Faculty and advisors"
+                                    className="md:w-full h-[220px] object-cover"
+                                />
+                                <p className="text-isd-font-3 font-bold">
+                                    Rob SCHARFF
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    &nbsp;
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    Assistant Professor
+                                </p>
+                            </div>
+                            <div className="flex flex-col items-center text-center">
+                                <Image
+                                    src={Jacleung}
+                                    alt="Faculty and advisors"
+                                    className="md:w-full h-[220px] object-cover"
+                                />
+                                <p className="text-isd-font-3 font-bold">
+                                    Jac LEUNG
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    &nbsp;
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    Lecturer
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 )}
 
                 {language === LANGUAGE_CHINESE_SIMPLIFIED && (
-                    <Image src={FacAdv1CnSimp} alt="师资队伍与顾问委员会" />
+                    <div className="flex flex-col  gap-4">
+                        {/* <Image src={FacAdv1} alt="Faculty and advisors" />*/}
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-start max-w-4xl mx-auto px-4">
+                            <div className="flex flex-col items-center text-center">
+                                <Image
+                                    src={Qianzhang}
+                                    alt="Faculty and advisors"
+                                    className=" object-cover h-[250px]"
+                                />
+                                <p className="text-isd-font-3 font-bold">
+                                    张黔{' '}
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    综合系统与设计学部主任
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    腾讯工程学教授 及 计算机科学与工程系讲座教授
+                                </p>
+                            </div>
+                            <div className="flex flex-col items-center text-center">
+                                <Image
+                                    src={Zexiangli}
+                                    alt="Faculty and advisors"
+                                    className=" object-cover h-[250px]"
+                                />
+                                <p className="text-isd-font-3 font-bold">
+                                    李泽湘
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    &nbsp;
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    综合系统与设计学部教授
+                                </p>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-4 items-start">
+                            <div className="flex flex-col items-center text-center">
+                                <Image
+                                    src={Saikityeung}
+                                    alt="Faculty and advisors"
+                                    className="md:w-full h-[220px] object-cover"
+                                />
+                                <p className="text-isd-font-3 font-bold">
+                                    杨世杰
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    &nbsp;
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    教授
+                                </p>
+                            </div>
+                            <div className="flex flex-col items-center text-center">
+                                <Image
+                                    src={Winnieleung}
+                                    alt="Faculty and advisors"
+                                    className="md:w-full h-[220px] object-cover"
+                                />
+                                <p className="text-isd-font-3 font-bold">
+                                    梁淑慧
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    &nbsp;
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    工程教育副教授{' '}
+                                </p>
+                            </div>
+                            <div className="flex flex-col items-center text-center">
+                                <Image
+                                    src={Changyingxiang}
+                                    alt="Faculty and advisors"
+                                    className="md:w-full h-[220px] object-cover"
+                                />
+                                <p className="text-isd-font-3 font-bold">
+                                    向畅颖
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    &nbsp;
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    助理教授
+                                </p>
+                            </div>
+                            <div className="flex flex-col items-center text-center">
+                                <Image
+                                    src={Qijiashao}
+                                    alt="Faculty and advisors"
+                                    className="md:w-full h-[220px] object-cover"
+                                />
+                                <p className="text-isd-font-3 font-bold">
+                                    邵琪佳
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    &nbsp;
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    助理教授
+                                </p>
+                            </div>
+                            <div className="flex flex-col items-center text-center">
+                                <Image
+                                    src={Robscharff}
+                                    alt="Faculty and advisors"
+                                    className="md:w-full h-[220px] object-cover"
+                                />
+                                <p className="text-isd-font-3 font-bold">
+                                    Rob SCHARFF
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    &nbsp;
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    助理教授
+                                </p>
+                            </div>
+                            <div className="flex flex-col items-center text-center">
+                                <Image
+                                    src={Jacleung}
+                                    alt="Faculty and advisors"
+                                    className="md:w-full h-[220px] object-cover"
+                                />
+                                <p className="text-isd-font-3 font-bold">
+                                    梁嘉乐
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    &nbsp;
+                                </p>
+                                <p className="text-isd-font-3 font-bold">
+                                    讲师
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 )}
             </div>
         </div>
--- src/components/blocks/mphil-tie/ResearchAreasBlock.tsx
+++ src/components/blocks/mphil-tie/ResearchAreasBlock.tsx
@@ -11,6 +11,26 @@ import ResHealth from '@/assets/academics/mphil-in-tie/res.health.png';
 import ResEmer from '@/assets/academics/mphil-in-tie/res.emer.png';
 import ResMar from '@/assets/academics/mphil-in-tie/res.mar.png';
 
+import Res1 from '@/assets/academics/mphil-in-tie/res-area/1.svg';
+import Res1En from '@/assets/academics/mphil-in-tie/res-area/1.en.svg';
+import Res1Cn from '@/assets/academics/mphil-in-tie/res-area/1.cn.svg';
+
+import Res2 from '@/assets/academics/mphil-in-tie/res-area/2.svg';
+import Res2En from '@/assets/academics/mphil-in-tie/res-area/2.en.svg';
+import Res2Cn from '@/assets/academics/mphil-in-tie/res-area/2.cn.svg';
+
+import Res3 from '@/assets/academics/mphil-in-tie/res-area/3.svg';
+import Res3En from '@/assets/academics/mphil-in-tie/res-area/3.en.svg';
+import Res3Cn from '@/assets/academics/mphil-in-tie/res-area/3.cn.svg';
+
+import Res4 from '@/assets/academics/mphil-in-tie/res-area/4.svg';
+import Res4En from '@/assets/academics/mphil-in-tie/res-area/4.en.svg';
+import Res4Cn from '@/assets/academics/mphil-in-tie/res-area/4.cn.svg';
+
+import Res5 from '@/assets/academics/mphil-in-tie/res-area/5.svg';
+import Res5En from '@/assets/academics/mphil-in-tie/res-area/5.en.svg';
+import Res5Cn from '@/assets/academics/mphil-in-tie/res-area/5.cn.svg';
+
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
@@ -71,60 +91,112 @@ export default function ResearchAreasBlock() {
 
             <div className="flex flex-col gap-component-gap-sm">
                 {(language === LANGUAGE_ENGLISH || !language) && (
-                    <div className="flex gap-component-gap">
+                    <div className="flex flex-col md:flex-row gap-component-gap ">
                         <div className="flex flex-col gap-section-gap">
                             <a href="/research/#design-tech">
-                                <Image src={ResDes} alt="Design tech" />
+                                <Image src={Res1} alt="Design tech"  className=" mx-auto"
+                                />
+                                <Image
+                                    src={Res1En}
+                                    alt="Design tech"
+                                    className="mt-[20px] mx-auto"
+                                />
                             </a>
                             <a href="/research/#marine-tech">
-                                <Image src={ResMar} alt="Marine tech" />
+                                <Image src={Res4} alt="Marine tech"  className=" mx-auto"
+                                />
+                                <Image
+                                    src={Res4En}
+                                    alt="Marine tech"
+                                    className="mt-[20px] mx-auto"
+                                />
                             </a>
                         </div>
                         <div className="flex flex-col justify-center">
                             <a href="/research/#sustainable-tech">
-                                <Image src={ResSus} alt="Sustainable tech" />
+                                <Image
+                                    src={Res3}
+                                    alt="Sustainable tech"
+                                    className=" mx-auto"
+                                />
+                                <Image
+                                    src={Res3En}
+                                    alt="Sustainable tech"
+                                    className="mt-[20px] mx-auto"
+                                />
                             </a>
                         </div>
                         <div className="flex flex-col gap-section-gap">
                             <a href="/research/#health-tech">
-                                <Image src={ResHealth} alt="Health tech" />
+                                <Image src={Res2} alt="Health tech"  className=" mx-auto"
+                                />
+                                <Image
+                                    src={Res2En}
+                                    alt="Health tech"
+                                    className="mt-[20px] mx-auto"
+                                />
                             </a>
                             <a href="/research/#emergent-themes">
-                                <Image src={ResEmer} alt="Emergent themes" />
+                                <Image src={Res5} alt="Emergent themes"  className=" mx-auto"
+                                />
+                                <Image
+                                    src={Res5En}
+                                    alt="Emergent themes"
+                                    className="mt-[20px] mx-auto"
+                                />
                             </a>
                         </div>
                     </div>
                 )}
 
                 {language === LANGUAGE_CHINESE_SIMPLIFIED && (
-                    <div className="flex gap-component-gap">
+                      <div className="flex flex-col md:flex-row gap-component-gap ">
                         <div className="flex flex-col gap-section-gap">
                             <a href="/research/#design-tech">
-                                <Image src={ResDesCnSimp} alt="Design tech" />
+                                <Image src={Res1} alt="Design tech"  className=" mx-auto"
+                                />
+                                <Image
+                                    src={Res1Cn}
+                                    alt="Design tech"
+                                    className="mt-[20px] mx-auto"
+                                />
                             </a>
                             <a href="/research/#marine-tech">
-                                <Image src={ResMarCnSimp} alt="Marine tech" />
+                                <Image src={Res4} alt="Marine tech"  className=" mx-auto"
+                                />
+                                <Image
+                                    src={Res4Cn}
+                                    alt="Marine tech"
+                                    className="mt-[20px] mx-auto"
+                                />
                             </a>
                         </div>
                         <div className="flex flex-col justify-center">
                             <a href="/research/#sustainable-tech">
+                                <Image src={Res3} alt="Sustainable tech"  className=" mx-auto"
+                                />
                                 <Image
-                                    src={ResSusCnSimp}
+                                    src={Res3Cn}
                                     alt="Sustainable tech"
+                                    className="mt-[20px] mx-auto"
                                 />
                             </a>
                         </div>
                         <div className="flex flex-col gap-section-gap">
                             <a href="/research/#health-tech">
+                                <Image src={Res2} alt="Health tech" className=" mx-auto"/>
                                 <Image
-                                    src={ResHealthCnSimp}
+                                    src={Res2Cn}
                                     alt="Health tech"
+                                    className="mt-[20px] mx-auto"
                                 />
                             </a>
                             <a href="/research/#emergent-themes">
+                                <Image src={Res5} alt="Emergent themes"className=" mx-auto"/>
                                 <Image
-                                    src={ResEmerCnSimp}
+                                    src={Res5Cn}
                                     alt="Emergent themes"
+                                    className="mt-[20px] mx-auto"
                                 />
                             </a>
                         </div>
