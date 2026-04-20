1304dddab510d8660ef91210eac2cbf7bb5166f6
Remi ANNAT <remi_annat@hotmail.com>
2026-04-20 09:36:24 +0800

    Change pictures to svg

--- src/components/blocks/mphil-tie/FacultyAdvisorsBlock.tsx
+++ src/components/blocks/mphil-tie/FacultyAdvisorsBlock.tsx
@@ -1,5 +1,15 @@
 import FacAdv1 from '@/assets/academics/mphil-in-tie/fac-adv.1.png';
 import FacAdv1CnSimp from '@/assets/academics/mphil-in-tie/fac-adv.1.cn.simp.png';
+
+import Changyingxiang from '@/assets/academics/mphil-in-tie/fac-adv/changyingxiang.svg';
+import Jacleung from '@/assets/academics/mphil-in-tie/fac-adv/jacleung.svg';
+import Qianzhang from '@/assets/academics/mphil-in-tie/fac-adv/qianzhang.svg';
+import Qijiashao from '@/assets/academics/mphil-in-tie/fac-adv/qijiashao.svg';
+import Robscharff from '@/assets/academics/mphil-in-tie/fac-adv/robscharff.svg';
+import Saikityeung from '@/assets/academics/mphil-in-tie/fac-adv/saikityeung.svg';
+import Winnieleung from '@/assets/academics/mphil-in-tie/fac-adv/winnieleung.svg';
+import Zexiangli from '@/assets/academics/mphil-in-tie/fac-adv/zexiangli.svg';
+
 import Image from 'next/image';
 import { LANGUAGE_CHINESE_SIMPLIFIED, LANGUAGE_ENGLISH } from '@/app/constants';
 import { useEffect, useState } from 'react';
@@ -22,7 +32,34 @@ export default function FacultyAdvisorsBlock() {
 
             <div className="flex flex-col gap-component-gap-sm">
                 {(language === LANGUAGE_ENGLISH || !language) && (
-                    <Image src={FacAdv1} alt="Faculty and advisors" />
+                    <>
+                        <Image src={FacAdv1} alt="Faculty and advisors" />
+                        {/*     <div className="flex">
+                            <Image src={Qianzhang} alt="Faculty and advisors" />
+                            <Image src={Zexiangli} alt="Faculty and advisors" />
+                        </div>
+                        <div className="flex">
+                            <Image
+                                src={Saikityeung}
+                                alt="Faculty and advisors"
+                            />
+                            <Image
+                                src={Winnieleung}
+                                alt="Faculty and advisors"
+                            />
+                            <Image
+                                src={Changyingxiang}
+                                alt="Faculty and advisors"
+                            />
+                            <Image src={Qijiashao} alt="Faculty and advisors" />
+                            <Image
+                                src={Robscharff}
+                                alt="Faculty and advisors"
+                            />
+
+                            <Image src={Jacleung} alt="Faculty and advisors" />
+                        </div>*/}
+                    </>
                 )}
 
                 {language === LANGUAGE_CHINESE_SIMPLIFIED && (
