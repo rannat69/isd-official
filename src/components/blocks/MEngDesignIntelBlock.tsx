5515f0209dab322a606f0965820199abe9d798f8
Remi Annat <remi_annat@hotmail.com>
2026-09-18 19:10:04 +0800

    Change %

--- src/components/blocks/MEngDesignIntelBlock.tsx
+++ src/components/blocks/MEngDesignIntelBlock.tsx
@@ -350,7 +350,7 @@ export default function MEngDesignIntelBlock() {
                             Program Fee
                         </h3>
 
-                        <div className="w-full  md:w-[90%] lg:w-[85%] overflow-x-auto mx-auto">
+                        <div className="w-full  md:w-[90%] lg:w-[80%] overflow-x-auto mx-auto">
                             <table className="w-full table-auto ">
                                 <tbody className="leading-[16px] text-[12px]">
                                     <tr className="bg-isd-primary-2 text-isd-primary text-md">
@@ -466,7 +466,7 @@ export default function MEngDesignIntelBlock() {
                                 For 2027/28 Fall Term Intake (commencing in Sep
                                 2027):
                             </p>
-                            <div className="w-full  md:w-[90%] lg:w-[85%] overflow-x-auto mx-auto">
+                            <div className="w-full  md:w-[90%] lg:w-[80%] overflow-x-auto mx-auto">
                                 <table className="w-full  table-auto ">
                                     <tbody className="leading-[16px] text-[12px]">
                                         <tr className="bg-isd-primary-2 text-isd-primary text-md">
@@ -519,7 +519,7 @@ export default function MEngDesignIntelBlock() {
                         </h3>
 
                         <div className="flex-1 flex flex-col gap-[12px]">
-                            <div className="w-full  md:w-[90%] lg:w-[85%] overflow-x-auto mx-auto">
+                            <div className="w-full  md:w-[90%] lg:w-[80%] overflow-x-auto mx-auto">
                                 <table className="w-full table-auto ">
                                     <tbody className="leading-[16px] text-[12px]">
                                         <tr className="bg-isd-primary-2 text-isd-primary text-md text-left">
